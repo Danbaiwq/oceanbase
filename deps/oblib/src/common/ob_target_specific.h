b6fc817d026e6294abf2a4d5b2b42be5e5342cc2
zoriswang <zoriswang@U-RQ7LL4QM-1904.local>
2025-06-23 13:09:12 +0800

    add neon dist func in ob
    Signed-off-by: zoriswang <zoriswang@U-RQ7LL4QM-1904.local>

--- deps/oblib/src/common/ob_target_specific.h
+++ deps/oblib/src/common/ob_target_specific.h
@@ -26,7 +26,8 @@ enum ObTargetArch : uint32_t
   SSE42    = (1 << 0),
   AVX      = (1 << 1),
   AVX2     = (1 << 2),
-  AVX512    = (1 << 3),
+  AVX512   = (1 << 3),
+  NEON     = (1 << 4),
 };
 
 bool is_arch_supported(ObTargetArch arch);
--- src/share/vector_type/ob_vector_op_common.h
+++ src/share/vector_type/ob_vector_op_common.h
@@ -23,6 +23,11 @@
 #include <nmmintrin.h>
 #endif
 
+#if defined (__aarch64__)
+#include <arm_neon.h>
+#endif
+
+
 namespace oceanbase
 {
 namespace common
